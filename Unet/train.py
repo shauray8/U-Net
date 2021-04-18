15923f527508ec7ee02a1100facd9a3bf58816b3
Shauray <shauray9@gmail.com>
2021-04-18 23:38:31 +0530

    writing the training loop

--- Unet/train.py
+++ Unet/train.py
@@ -1,2 +1,25 @@
 import torch
+import logging
+import sys, os
+
+import numpy as np
+from torch import optimm
+from tqdm import trange
+
+from model import UNet
+
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import DataLoader, random_split
+from utils import *
+
+
+# TBD
+img_dir = ""
+mask_dir = ""
+checkpoints = ""
+
+
+def Train_this_mf(net, device, epochs, batch_size, lr, val_per=.1, save_cp=True, img_scale=.5):
+
+    dataset = "   "
 
