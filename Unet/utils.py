48238d0b3ca8ae926992d3c3bd067b74378d8058
Shauray <shauray9@gmail.com>
2021-04-19 00:18:18 +0530

    preprocessing the dataset

--- Unet/utils.py
+++ Unet/utils.py
@@ -1,6 +1,17 @@
 import torch
 import os
+import numpy as np
+from glob import glob
+import logging
+from torch.utils.data import Dataset
+from PIL import Image
 
+class preproccesing(dataset):
+    def __init__(self, imgs_dir, masks_dir, mask_suffix=""):
+        pass
+
+    def __getitem__(self, i):
+        pass
 
 if __name__ == "__main__":
     print("utility")
