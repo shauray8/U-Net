d6d35a61377c4bf26bdfdd6c17e94f9b0dedcfc2
Shauray <shauray9@gmail.com>
2021-04-19 00:32:13 +0530

    loading the data

--- Unet/utils.py
+++ Unet/utils.py
@@ -5,13 +5,23 @@ from glob import glob
 import logging
 from torch.utils.data import Dataset
 from PIL import Image
+import matplotlib.pyplot as plt
+
 
 class preproccesing(dataset):
     def __init__(self, imgs_dir, masks_dir, mask_suffix=""):
-        pass
+        # temp solution 
+        for i in imgs_dir:
+            for j in i:
+                im = Image.open(f"{j}.tif") 
+                imarray = numpy.array(im) 
+                imarray.shape 
+                plt.imshow(imarray)
+                plt.show()
 
     def __getitem__(self, i):
         pass
 
 if __name__ == "__main__":
     print("utility")
+    
