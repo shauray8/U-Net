4e1ef27526ef2afb12330d1afa9e4f484de920b6
Shauray <shauray9@gmail.com>
2021-04-23 01:00:04 +0530

    dont know

--- Unet/utils.py
+++ Unet/utils.py
@@ -22,6 +22,20 @@ class preproccesing(dataset):
     def __getitem__(self, i):
         pass
 
+class corona_dataset(Dataset):
+    def __init__(self, imgs_dir, masks_dir, scale=1):
+        self.imags_dir = imgs_dir
+        self.masks_dir = masks_dir
+        self.scale = scale
+
+        assert 0 < scale <= 1, 'scale must be between 0 and 1'
+
+        self.ids = [splitext(file)[0] for file in listdir(imgs)
+                if not file.startwidth('.')]
+
+        logging.info(f"Createing dataset with {len(self.ids)} example")
+
+
 if __name__ == "__main__":
     print("utility")
     
